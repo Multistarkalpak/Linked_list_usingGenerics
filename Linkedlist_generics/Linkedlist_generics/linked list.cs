5e14731dc4c587702ba6d3f959d7ed6a1cad68b4
Multistarkalpak <105732959+Multistarkalpak@users.noreply.github.com>
2022-06-24 18:27:14 +0530

    Inserting the number at mid

--- Linkedlist_generics/Linkedlist_generics/linked list.cs
+++ Linkedlist_generics/Linkedlist_generics/linked list.cs
@@ -16,22 +16,82 @@ namespace Day14_LinkedList
             Head = null;
             Tail = null;
         }
-
+        public void AddNode(int value)
+        {
+            Node node = new Node(value);
+            //Check wheather the list is empty then creates node as head
+            if (Head == null & Tail == null)
+            {
+                Head = node;
+                Tail = node;
+            }
+            else
+            {
+                ////If node than add new node as head 
+                node.next = Head;
+                Head = node;
+            }
+            Console.WriteLine($" Added {value} at start of the linkedlist");
+        }
+        public void AppendNode(int value)
+        {
+            Node node = new Node(value);
+            //Check wheather the list is empty then creates node as head
+            if (Head == null & Tail == null)
+            {
+                Head = node;
+                Tail = node;
+            }
+            else
+            {
+                Node temp = Head;
+                ////If node than add new node as head 
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine($"Added {value} at start of the linkedlist");
+        }
+        public void InsertNode(int value, int Previousvalue)
+        {
+            Node newNode = new Node(value);
+            if (Head == null & Tail == null)
+            {
+                Head = newNode;
+            }
+            else
+            {
+                Node temp = Head;
+                //Checking until temp variable data equal to the previous value of value to be added
+                while (temp.data != Previousvalue)
+                {
+                    temp = temp.next;
+                }
+                newNode.next = temp.next;
+                temp.next = newNode;
+            }
+        }
         internal void Display()
         {
-
+            //temp variable will head
             Node temp = Head;
+            //Check wheather list is empty or not 
             if (temp == null)
                 Console.WriteLine("Linked list is empty");
             else
                 Console.WriteLine("Linked List:");
             while (temp != null)
             {
+                //Check until temp will null
                 Console.Write(temp.data);
                 if (temp.next != null)
                     Console.Write(" -> ");
+                //for traversing the node
                 temp = temp.next;
             }
+
         }
     }
 }
